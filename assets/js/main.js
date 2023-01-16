933333cea37e0ac468fa1a4a43b6485c44ca65a9
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-16 19:34:22 +0100

    BurgerMenue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,39 @@
 console.log("test")
+
+
+/* 
+to do
+
+wenn a butten_burgerMenue gedrückt wird
+
+
+
+
+
+
+
+
+*/
+
+let burgerMenue___display = document.getElementById("burgerMenue___display");
+
+let button___burgerMenue = document.getElementById("button___burgerMenue");
+
+button___burgerMenue.addEventListener("click", toggleBurgerMenue);
+
+function toggleBurgerMenue() {
+
+    burgerMenue___display.classList.toggle("js_toggle_button_burgerMenue___display");
+
+    /*
+    klappt top
+    
+    if (burgerMenue___display.style.display === "none") {
+        burgerMenue___display.style.display = "flex";
+    } else {
+        burgerMenue___display.style.display = "none";
+    }
+     */
+
+
+}
